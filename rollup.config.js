a157b6bb8826b142d57645ecc4d0525d7f38c324
Allain Lalonde <alalonde@elastalink.com>
2019-07-18 21:00:23 -0400

    making single script for all build artifacts using a rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs"
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm"
+      }
+    ]
+  },
+  {
+    input: "src/bin.js",
+    output: {
+      file: "dist/bin.js",
+      format: "cjs"
+    }
+  }
+]
