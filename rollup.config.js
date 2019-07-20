f11b1919aa841269a0b43b1f15d78da0adc8ea0f
Allain Lalonde <alalonde@elastalink.com>
2019-07-20 01:08:15 -0400

    removing bin build

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,13 @@
-import resolve from "rollup-plugin-node-resolve"
-
-export default [
-  {
-    input: "src/index.js",
-    output: [
-      {
-        file: "dist/index.cjs.js",
-        format: "cjs"
-        // exports: "named"
-      },
-      {
-        file: "dist/index.esm.js",
-        format: "esm"
-        // exports: "named"
-      }
-    ]
-    // plugins: [resolve()]
-  },
-  {
-    input: "src/bin.js",
-    output: {
-      file: "dist/bin.js",
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.cjs.js",
       format: "cjs"
-      // exports: "named"
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm"
     }
-    // plugins: [resolve()]
-  }
-]
+  ]
+}
