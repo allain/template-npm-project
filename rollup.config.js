31111caad96d013b4e6372b2edae0111f050f500
Allain Lalonde <alalonde@elastalink.com>
2019-07-23 13:04:03 -0400

    Adding npm run dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
+import typescript from "rollup-plugin-typescript"
+
 export default {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: [
     {
       file: "dist/index.cjs.js",
@@ -9,5 +11,6 @@ export default {
       file: "dist/index.esm.js",
       format: "esm"
     }
-  ]
+  ],
+  plugins: [typescript()]
 }
--- tests/working.test.js
+++
@@ -1,7 +0,0 @@
-import os from "os"
-
-describe("tests", () => {
-  it("works!", () => {
-    expect(os.tmpdir()).toBeDefined()
-  })
-})
